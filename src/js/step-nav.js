e93b333114c6240864a55f70c3b4e7fa18ce5f34
Patryk <patryk.cielen@gmail.com>
2023-06-05 19:39:20 +0200

    JS Step-2

--- src/js/step-nav.js
+++ src/js/step-nav.js
@@ -3,7 +3,10 @@ let mainSteps
 let prevBtn
 let nextBtn
 
-let selections = {}
+let selections = {
+	plan: '',
+	period: '',
+}
 
 let step1Inputs
 let step1Warnings
@@ -37,7 +40,7 @@ const prepareDOMEvents = () => {
 	navSteps.forEach(step => step.addEventListener('click', navStepSelect))
 	nextBtn.addEventListener('click', stepNext)
 	prevBtn.addEventListener('click', stepPrev)
-	nextBtn.addEventListener('click', ifEmpty)
+	nextBtn.addEventListener('click', ifCorrect1)
 	// step2Inputs.forEach(plan => plan.addEventListener('click', console.log('step2 input dziala')))
 	step2Inputs.forEach(plan => plan.addEventListener('click', planSelect))
 	step2Switch.querySelector('input').addEventListener('click', periodSelect)
@@ -78,8 +81,11 @@ const activateStep = forward => {
 }
 
 const stepNext = () => {
-	if (currentStep() < 3 && ifEmpty() == true) {
-		activateStep(currentStep() + 1)
+	if (currentStep() < 3 && ifCorrect1() == true) {
+		activateStep(1)
+	}
+	if (currentStep() < 3 && ifCorrect2() == true) {
+		activateStep(2)
 	}
 }
 const stepPrev = () => {
@@ -94,7 +100,7 @@ const navStepSelect = e => {
 
 // 1 STEP
 
-const ifEmpty = () => {
+const ifCorrect1 = () => {
 	let wrongInputs = 0
 	for (let i = 0; i < step1Inputs.length; i++) {
 		if (step1Inputs[i].value.trim().length != 0) {
@@ -119,10 +125,15 @@ const removeActive = input => {
 	input.classList.remove('active')
 }
 
+const uncheck = () => {
+	step2Switch.querySelector('input').checked = false
+	selections.period = 'Monthly'
+}
+
 const planSelect = e => {
 	step2Inputs.forEach(input => removeActive(input))
 	addActive(e.target)
-	console.log(e.target);
+	console.log(e.target)
 	let planName = e.target.querySelector('h2').textContent
 	selections.plan = planName
 	console.log(selections)
@@ -131,16 +142,41 @@ const planSelect = e => {
 const periodSelect = () => {
 	if (step2Switch.querySelector('input').checked) {
 		selections.period = 'Yearly'
+		step2Switch.querySelectorAll('div')[2].classList.toggle('selected')
+		step2Switch.querySelectorAll('div')[0].classList.toggle('selected')
+		// step2Inputs.querySelector('div h2')textContent('hejka')
+
+		step2Inputs[0].querySelectorAll('div p')[0].textContent = '$90/yr'
+		step2Inputs[1].querySelectorAll('div p')[0].textContent = '$120/yr'
+		step2Inputs[2].querySelectorAll('div p')[0].textContent = '$150/yr'
+
+		step2Inputs.forEach(input => (input.querySelector('.bonus').style.display = 'flex'))
 	} else {
-		console.log('miesiecznie')
 		selections.period = 'Monthly'
+		step2Switch.querySelectorAll('div')[2].classList.toggle('selected')
+		step2Switch.querySelectorAll('div')[0].classList.toggle('selected')
+
+		step2Inputs[0].querySelectorAll('div p')[0].textContent = '$9/mo'
+		step2Inputs[1].querySelectorAll('div p')[0].textContent = '$12/mo'
+		step2Inputs[2].querySelectorAll('div p')[0].textContent = '$15/mo'
+
+		step2Inputs.forEach(input => (input.querySelector('.bonus').style.display = 'none'))
 	}
 	console.log(selections)
 	// console.log(step2Switch.querySelector('input').checked)
+}
+
+const ifCorrect2 = () => {
+	if (selections.plan.length === 0 || selections.period.length === 0) {
+		return false
+	} else {
+		return true
+	}
 }
 
 document.addEventListener('DOMContentLoaded', () => {
 	main()
 	test()
 	currentStep()
+	uncheck()
 })
