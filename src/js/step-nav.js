d52c94769479b4ccdf882dc6facaec8a9ef338e1
Patryk <patryk.cielen@gmail.com>
2023-06-09 20:25:49 +0200

    JS Step-3

--- src/js/step-nav.js
+++ src/js/step-nav.js
@@ -3,15 +3,13 @@ let mainSteps
 let prevBtn
 let nextBtn
 
-let selections = {
-	plan: '',
-	period: '',
-}
+let selections = {}
 
 let step1Inputs
 let step1Warnings
 let step2Inputs
 let step2Switch
+let step3Checkboxes
 
 const main = () => {
 	prepareDOMElements()
@@ -34,21 +32,23 @@ const prepareDOMElements = () => {
 
 	step2Inputs = document.querySelectorAll('.step-2 .input-plan')
 	step2Switch = document.querySelector('.step-2 .period-switch')
+
+	step3Checkboxes = document.querySelectorAll('.step-3 input')
 }
 
 const prepareDOMEvents = () => {
 	navSteps.forEach(step => step.addEventListener('click', navStepSelect))
 	nextBtn.addEventListener('click', stepNext)
 	prevBtn.addEventListener('click', stepPrev)
-	nextBtn.addEventListener('click', ifCorrect1)
-	// step2Inputs.forEach(plan => plan.addEventListener('click', console.log('step2 input dziala')))
 	step2Inputs.forEach(plan => plan.addEventListener('click', planSelect))
 	step2Switch.querySelector('input').addEventListener('click', periodSelect)
+	// step3Checkboxes.querySelector('input[type="checkbox"]').addEventListener('click', addOnSelect)
+	step3Checkboxes.forEach(addon => addon.addEventListener('click', addOnSelect))
 }
 
 const test = () => {}
-// console.log(step2Inputs)
-//STEP NAVIGATION
+
+//STEP NAVIGATION and START FUNCS
 
 const currentStep = () => {
 	for (let i = 0; i < navSteps.length; i++) {
@@ -81,11 +81,36 @@ const activateStep = forward => {
 }
 
 const stepNext = () => {
-	if (currentStep() < 3 && ifCorrect1() == true) {
-		activateStep(1)
-	}
-	if (currentStep() < 3 && ifCorrect2() == true) {
-		activateStep(2)
+	// if (currentStep() < 3 && ifCorrect1() == true) {
+	// 	activateStep(1)
+	// }
+	// if (currentStep() < 3 && ifCorrect2() == true) {
+	// 	activateStep(2)
+	// }
+
+	// if (currentStep() < 3 && ifCorrect3() == true) {
+	// 	activateStep(3)
+	// }
+
+	// if (currentStep() == 0 && ifCorrect1() == true) {
+	// 	activateStep(currentStep() + 1)
+	// }
+	// if (currentStep() == 1 && ifCorrect2() == true) {
+	// 	activateStep(currentStep() + 1)
+	// }
+	// if (currentStep() == 2 && ifCorrect3() == true) {
+	// 	activateStep(currentStep() + 1)
+	// }
+	if (ifCorrect1() == false && currentStep() == 0) {
+		console.log('warunek 1 niespelniony')
+	} else {
+		if (ifCorrect2() == false && currentStep() == 1) {
+			console.log('warunek 2 niespelniony')
+		} else {
+			if (ifCorrect3() == false && currentStep() == 2) {
+				console.log('warunek 3 niespelniony')
+			} else activateStep(currentStep() + 1)
+		}
 	}
 }
 const stepPrev = () => {
@@ -96,6 +121,22 @@ const stepPrev = () => {
 
 const navStepSelect = e => {
 	activateStep(e.target.textContent - 1)
+}
+
+// COMMON
+const wipeout = () => {
+	step2Switch.querySelector('input').checked = false
+	selections.period = 'Monthly'
+	step3Checkboxes.forEach(checkbox => (checkbox.checked = false))
+	delete selections.addon1
+	delete selections.addon2
+	delete selections.addon3
+}
+const addActive = input => {
+	input.classList.add('active')
+}
+const removeActive = input => {
+	input.classList.remove('active')
 }
 
 // 1 STEP
@@ -118,22 +159,10 @@ const ifCorrect1 = () => {
 }
 
 // 2 STEP
-const addActive = input => {
-	input.classList.add('active')
-}
-const removeActive = input => {
-	input.classList.remove('active')
-}
-
-const uncheck = () => {
-	step2Switch.querySelector('input').checked = false
-	selections.period = 'Monthly'
-}
 
 const planSelect = e => {
 	step2Inputs.forEach(input => removeActive(input))
 	addActive(e.target)
-	console.log(e.target)
 	let planName = e.target.querySelector('h2').textContent
 	selections.plan = planName
 	console.log(selections)
@@ -144,13 +173,16 @@ const periodSelect = () => {
 		selections.period = 'Yearly'
 		step2Switch.querySelectorAll('div')[2].classList.toggle('selected')
 		step2Switch.querySelectorAll('div')[0].classList.toggle('selected')
-		// step2Inputs.querySelector('div h2')textContent('hejka')
 
 		step2Inputs[0].querySelectorAll('div p')[0].textContent = '$90/yr'
 		step2Inputs[1].querySelectorAll('div p')[0].textContent = '$120/yr'
 		step2Inputs[2].querySelectorAll('div p')[0].textContent = '$150/yr'
 
 		step2Inputs.forEach(input => (input.querySelector('.bonus').style.display = 'flex'))
+
+		step3Checkboxes[0].closest('.input-addon').querySelector('.price').textContent = '$10/yr'
+		step3Checkboxes[1].closest('.input-addon').querySelector('.price').textContent = '$20/yr'
+		step3Checkboxes[2].closest('.input-addon').querySelector('.price').textContent = '$20/yr'
 	} else {
 		selections.period = 'Monthly'
 		step2Switch.querySelectorAll('div')[2].classList.toggle('selected')
@@ -161,22 +193,58 @@ const periodSelect = () => {
 		step2Inputs[2].querySelectorAll('div p')[0].textContent = '$15/mo'
 
 		step2Inputs.forEach(input => (input.querySelector('.bonus').style.display = 'none'))
+
+		step3Checkboxes[0].closest('.input-addon').querySelector('.price').textContent = '$1/mo'
+		step3Checkboxes[1].closest('.input-addon').querySelector('.price').textContent = '$2/mo'
+		step3Checkboxes[2].closest('.input-addon').querySelector('.price').textContent = '$2/mo'
 	}
 	console.log(selections)
-	// console.log(step2Switch.querySelector('input').checked)
 }
 
 const ifCorrect2 = () => {
-	if (selections.plan.length === 0 || selections.period.length === 0) {
+	if (selections.plan === undefined || selections.period === undefined) {
 		return false
 	} else {
 		return true
 	}
 }
 
+// 3 STEP
+
+const addOnSelect = e => {
+	// console.log(e.target);
+
+	if (step3Checkboxes[0].checked == true) {
+		selections.addon1 = 'Online services'
+		step3Checkboxes[0].closest('.input-addon').classList.add('active')
+	} else {
+		delete selections.addon1
+		step3Checkboxes[0].closest('.input-addon').classList.remove('active')
+	}
+	if (step3Checkboxes[1].checked == true) {
+		selections.addon2 = 'Larger storage'
+		step3Checkboxes[1].closest('.input-addon').classList.add('active')
+	} else {
+		delete selections.addon2
+		step3Checkboxes[1].closest('.input-addon').classList.remove('active')
+	}
+	if (step3Checkboxes[2].checked == true) {
+		selections.addon3 = 'Customizable profile'
+		step3Checkboxes[2].closest('.input-addon').classList.add('active')
+	} else {
+		delete selections.addon3
+		step3Checkboxes[2].closest('.input-addon').classList.remove('active')
+	}
+	console.log(selections)
+}
+
+const ifCorrect3 = () => {
+	return true
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 	main()
 	test()
 	currentStep()
-	uncheck()
+	wipeout()
 })
